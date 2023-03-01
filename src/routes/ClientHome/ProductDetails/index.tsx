c98e1a1b3e34a54eb7d15f3831cdc3300bc2faf3
Rodrigo Ramos <digojramos@gmail.com>
2023-03-01 11:46:32 -0300

    Cart clear

--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -7,6 +7,7 @@ import { useNavigate, useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { ProductDTO } from "../../../models/product";
+import * as carttService from "../../../services/cart-service";
 
 export default function ProductDetails() {
   const params = useParams();
@@ -26,12 +27,21 @@ export default function ProductDetails() {
       });
   }, []);
 
+  function handleBuyClick() {
+    if(product) {
+      carttService.addProduct(product);
+      navigate("/cart");
+    }
+  }
+
   return (
     <main>
       <section id="product-details-section" className="dsc-container">
         {product && <ProductDetailsCard product={product} />}
         <div className="dsc-btn-page-container">
-          <ButtonPrimary text="Comprar" />
+          <div onClick={handleBuyClick}>
+            <ButtonPrimary text="Comprar" />
+          </div>
           <Link to="/">
             <ButtonInverse text="Início" />
           </Link>
