136a2c3102f5f60c80a31ba8359a4caab4e4c05c
Rodrigo Ramos <digojramos@gmail.com>
2023-03-07 10:21:15 -0300

    PrivateRoute

--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -5,7 +5,6 @@ import ButtonNextPage from "../../../components/ButtonNextPage";
 import * as productService from "../../../services/product-service";
 import { useEffect, useState } from "react";
 import { ProductDTO } from "../../../models/product";
-import { isAuthenticated } from "../../../services/auth-service";
 
 type QueryParams = {
   page: number;
@@ -24,8 +23,6 @@ export default function Catalog() {
   });
 
   useEffect(() => {
-    console.log("AUTENTICADO", isAuthenticated());
-
     productService.findPageRequest(queryParams.page, queryParams.name)
       .then(response => {
         const nextPage = response.data.content;
