0f206e3801a9e1016ca67571dc0e5e5c8243d1de
Rodrigo Ramos <digojramos@gmail.com>
2023-03-10 11:26:00 -0300

    Forms updateAll

--- src/routes/Admin/ProductListing/index.tsx
+++ src/routes/Admin/ProductListing/index.tsx
@@ -8,6 +8,8 @@ import SearchBar from '../../../components/SearchBar';
 import ButtonNextPage from '../../../components/ButtonNextPage';
 import DialogInfo from '../../../components/DialogInfo';
 import DialogConfirmation from '../../../components/DialogConfirmation';
+import ButtonInverse from '../../../components/ButtonInverse';
+import { useNavigate } from 'react-router-dom';
 
 type QueryParams = {
   page: number;
@@ -15,6 +17,8 @@ type QueryParams = {
 }
 
 export default function ProductListing() {
+
+  const navigate = useNavigate();
 
   const [dialogInfoData, setDialogInfoData] = useState({
     visible: false,
@@ -54,6 +58,10 @@ export default function ProductListing() {
     setQueryParams({...queryParams, page: queryParams.page + 1});
   }
 
+  function handleNewProductClick() {
+    navigate("/admin/products/create");
+  }
+
   function handleDialogInfoClose() {
     setDialogInfoData({...dialogInfoData, visible: false});
   }
@@ -85,7 +93,9 @@ export default function ProductListing() {
         <section id="product-listing-section" className="dsc-container">
           <h2 className="dsc-section-title dsc-mb20">Cadastro de produtos</h2>
           <div className="dsc-btn-page-container dsc-mb20">
-            <div className="dsc-btn dsc-btn-white">Novo</div>
+            <div onClick={handleNewProductClick}>
+              <ButtonInverse text="Novo"/>
+            </div>
           </div>
           <SearchBar onSearch={handleSearch}/>
           <table className="dsc-table dsc-mb20 dsc-mt20">
