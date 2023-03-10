b059006202ba4c60c47b12cc3bd68884a7c86b1e
Rodrigo Ramos <digojramos@gmail.com>
2023-03-10 11:49:02 -0300

    Forms validate

--- src/routes/Admin/ProductForm/index.tsx
+++ src/routes/Admin/ProductForm/index.tsx
@@ -26,6 +26,10 @@ export default function ProductForm() {
       name: "price",
       type: "number",
       placeholder: "Preço",
+      validation: function(value: any) {
+        return Number(value) > 0;
+      },
+      message: "Favor informar um valor positivo"
     },
     imgUrl: {
       value: "",
@@ -37,6 +41,8 @@ export default function ProductForm() {
   });
 
   useEffect(() => {
+    const obj = forms.validate(formData, "price");
+    console.log(obj);
     if (isEditing) {
       productService.findById(Number(params.productId))
         .then(response => {
