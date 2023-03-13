23b494372dce0a628abb582a6ceb1a848cba662e
Rodrigo Ramos <digojramos@gmail.com>
2023-03-13 11:34:47 -0300

    React Select Integration

--- src/routes/Admin/ProductForm/index.tsx
+++ src/routes/Admin/ProductForm/index.tsx
@@ -4,12 +4,17 @@ import { Link, useParams } from 'react-router-dom';
 import FormInput from '../../../components/FormInput';
 import * as forms from "../../../utils/forms";
 import * as productService from "../../../services/product-service";
+import * as categoryService from "../../../services/category-service";
 import FormTextArea from '../../../components/FormTextArea';
+import { CategoryDTO } from '../../../models/category';
+import FormSelect from '../../../components/FormSelect';
 
 
 export default function ProductForm() {
 
   const params = useParams();
+
+  const [categories, setCategories] = useState<CategoryDTO[]>([]);
 
   const isEditing = params.productId !== 'create';
 
@@ -53,8 +58,25 @@ export default function ProductForm() {
         return /^.{10,}$/.test(value);
       },
       message: "A descrição deve ter pelo menos 10 caracteres"
+    },
+    categories: {
+      value: [],
+      id: "categories",
+      name: "categories",
+      placeholder: "Categorias",
+      validation: function(value: CategoryDTO[]) {
+        return value.length > 0;
+      },
+      message: "Escolha ao menos uma categoria"
     }
   });
+
+  useEffect(() => {
+    categoryService.findAllRequest()
+      .then(response => {
+        setCategories(response.data);
+      });
+  }, [])
 
   useEffect(() => {
     if (isEditing) {
@@ -105,6 +127,22 @@ export default function ProductForm() {
                       onChange={handleInputChange}
                       onTurnDirty={handleTurnDirty}
                     />
+              </div>
+              <div>
+                <FormSelect 
+                  {...formData.categories}
+                  className="dsc-form-control"
+                  options={categories} 
+                  onChange={(obj: any) => {
+                    const newFormData = forms.updateAndValidate(formData, "categories", obj);
+                    setFormData(newFormData);
+                  }}
+                  onTurnDirty={handleTurnDirty}
+                  isMulti 
+                  getOptionLabel={(obj: any) => obj.name}
+                  getOptionValue={(obj: any) => String(obj.id)}
+                  />
+                  <div className="dsc-form-error">{formData.categories.message}</div>
               </div>
               <div>
                 <FormTextArea
